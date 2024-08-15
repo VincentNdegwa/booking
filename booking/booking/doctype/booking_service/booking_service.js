e123117ed6cc5c329079392ea03e6b95cc4d6dfe
VincentNdegwa <ndegwavincent7@gmail.com>
2024-08-15 23:43:32 +0300

    "Updated Booking Service and Booking Service Add-ons form scripts"

--- booking/booking/doctype/booking_service/booking_service.js
+++ booking/booking/doctype/booking_service/booking_service.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2024, vincent and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Booking Service", {
-// 	refresh(frm) {
+frappe.ui.form.on("Booking Service", {
+	refresh(frm) {},
+});
 
-// 	},
-// });
+frappe.ui.form.on("Booking Service Add-ons", {
+	price(frm, cdt, cdn) {
+		let total_add_on_price = 0.0;
+		frm.doc.add_ons.forEach((element) => {
+			total_add_on_price += element.price;
+		});
+		total_add_on_price += frm.doc.price;
+		frm.set_value("price", total_add_on_price);
+		console.log(frm);
+	},
+});
