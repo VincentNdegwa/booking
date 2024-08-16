108c80f9b555ed8f6b8067d7a5fb6e1db3c4cfd4
VincentNdegwa <ndegwavincent7@gmail.com>
2024-08-16 16:34:13 +0300

    "Updated Booking doctype: added 'Late' status, corrected 'Pending' capitalization; updated Booking Service doctype: added 'total_price' field, modified JavaScript to calculate total price; updated timestamps and modified_by fields."

--- booking/booking/doctype/booking_service/booking_service.js
+++ booking/booking/doctype/booking_service/booking_service.js
@@ -1,18 +1,27 @@
 // Copyright (c) 2024, vincent and contributors
 // For license information, please see license.txt
 
+function calculate_total_price(frm) {
+	let total_add_on_price = 0.0;
+	frm.doc.add_ons.forEach((element) => {
+		total_add_on_price += element.price;
+	});
+	total_add_on_price += frm.doc.price;
+	frm.set_value("total_price", total_add_on_price);
+}
+
 frappe.ui.form.on("Booking Service", {
 	refresh(frm) {},
+	price(frm) {
+		calculate_total_price(frm);
+	},
 });
 
 frappe.ui.form.on("Booking Service Add-ons", {
 	price(frm, cdt, cdn) {
-		let total_add_on_price = 0.0;
-		frm.doc.add_ons.forEach((element) => {
-			total_add_on_price += element.price;
-		});
-		total_add_on_price += frm.doc.price;
-		frm.set_value("price", total_add_on_price);
-		console.log(frm);
+		calculate_total_price(frm);
+	},
+	add_ons_remove(frm) {
+		calculate_total_price(frm);
 	},
 });
---
+++ booking/booking/doctype/reservation/reservation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, vincent and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Reservation", {
+// 	refresh(frm) {
+
+// 	},
+// });
