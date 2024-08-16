b8f3ff96516e7756da6e44f8f179243613251a27
VincentNdegwa <ndegwavincent7@gmail.com>
2024-08-16 19:00:41 +0300

    "Updated Reservation and Reservation Service doctypes: added calculate_reservation_price function, modified field types and properties, and updated timestamps."

--- booking/booking/doctype/reservation/reservation.js
+++ booking/booking/doctype/reservation/reservation.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2024, vincent and contributors
 // For license information, please see license.txt
+function calculate_reservation_price(frm) {
+	let total_reservation_price = 0;
+	frm.doc.services.forEach((element) => {
+		total_reservation_price += element.service_price;
+	});
+	frm.set_value("total_price", total_reservation_price);
+}
 
-// frappe.ui.form.on("Reservation", {
-// 	refresh(frm) {
+frappe.ui.form.on("Reservation", {
+	refresh(frm) {},
+});
 
-// 	},
-// });
+frappe.ui.form.on("Reservation Service", {
+	service_price(frm, cdt, cdn) {
+		calculate_reservation_price(frm);
+	},
+});
