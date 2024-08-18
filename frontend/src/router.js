855a97f53f20c294c50e66a5ce4d4a68a53fd7fb
VincentNdegwa <ndegwavincent7@gmail.com>
2024-08-18 12:19:31 +0300

    "Uncommented beforeEach router guard"

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -20,21 +20,21 @@ let router = createRouter({
 	routes,
 })
 
-// router.beforeEach(async (to, from, next) => {
-//   let isLoggedIn = session.isLoggedIn
-//   try {
-//     await userResource.promise
-//   } catch (error) {
-//     isLoggedIn = false
-//   }
+router.beforeEach(async (to, from, next) => {
+  let isLoggedIn = session.isLoggedIn
+  try {
+    await userResource.promise
+  } catch (error) {
+    isLoggedIn = false
+  }
 
-//   if (to.name === 'Login' && isLoggedIn) {
-//     next({ name: 'Home' })
-//   } else if (to.name !== 'Login' && !isLoggedIn) {
-//     next({ name: 'Login' })
-//   } else {
-//     next()
-//   }
-// })
+  if (to.name === 'Login' && isLoggedIn) {
+    next({ name: 'Home' })
+  } else if (to.name !== 'Login' && !isLoggedIn) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
+})
 
 export default router
