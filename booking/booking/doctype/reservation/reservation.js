c77dd53b9a43478be252f1273606981ac968e8c2
VincentNdegwa <ndegwavincent7@gmail.com>
2024-08-20 16:12:40 +0300

    "Updated Reservation doctype: modified JavaScript code to recalculate total amount and balance, and updated JSON file to add new fields and modify existing ones."

--- booking/booking/doctype/reservation/reservation.js
+++ booking/booking/doctype/reservation/reservation.js
@@ -2,24 +2,36 @@
 // For license information, please see license.txt
 function calculate_reservation_price(frm) {
 	let total_reservation_price = 0;
-	frm.doc.services.forEach((element) => {
-		total_reservation_price += element.service_price;
+	total_reservation_price += frm.doc.service_price;
+	let payed_amount = 0;
+	frm.doc.payments.forEach((element) => {
+		payed_amount += element.amount_paid;
 	});
-	frm.set_value("total_price", total_reservation_price);
+	frm.set_value("total_amount", total_reservation_price);
+	frm.set_value("total_amount_paid", payed_amount);
+	frm.set_value("balance_remaining", total_reservation_price - payed_amount);
 }
 
 frappe.ui.form.on("Reservation", {
-	refresh(frm) {},
+	refresh(frm) {
+		calculate_reservation_price(frm);
+	},
 	before_submit(frm) {
 		if (frm.doc.payment_status != "Paid") {
 			frappe.throw(__("The reservation is unpaid, please pay first before submitting!"));
 			return false;
 		}
 	},
+	service_price(frm) {
+		calculate_reservation_price(frm);
+	},
 });
 
 frappe.ui.form.on("Reservation Payment History", {
-	// service_price(frm, cdt, cdn) {
-	// 	calculate_reservation_price(frm);
-	// },
+	amount_paid(frm, cdt, cdn) {
+		calculate_reservation_price(frm);
+	},
+	payments_remove(frm) {
+		calculate_reservation_price(frm);
+	},
 });
